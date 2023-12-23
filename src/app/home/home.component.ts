6a48d3d0e0cb9ab25b985f4413ce68b16b38bed3
yasi-ayazi <yasi.ayazi@gmail.com>
2023-12-23 18:55:06 +0100

    adding housing locatimon component adding housing locatin service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: `  <section>
-  <form>
-    <input type="text" placeholder="Filter by city">
-    <button class="primary" type="button">Search</button>
-  </form>
-</section>`,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location
+    *ngFor="let housingLocation of housingLocationList"
+    [housingLocation] = "housingLocation"
+    ></app-housing-location>
+  </section>
+  `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+housingService: HousingService = inject(HousingService);
+constructor () {
+  this.housingLocationList= this.housingService.getAllHousingLocations();
+}
 
 }
+
+
